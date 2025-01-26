c886940399df5135ecc3ef2d1ced35cc58ed030d
alex_a <admin@modnica.com>
2025-01-26 19:54:36 +0300

    First commit

---
+++ src/main/java/spb/alex/App.java
@@ -0,0 +1,13 @@
+package spb.alex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
