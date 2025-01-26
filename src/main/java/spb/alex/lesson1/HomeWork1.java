00063ae06b9dbc0ad4bb4adb22cb05e5a8269af8
alex_a <admin@modnica.com>
2025-01-26 20:00:45 +0300

    HomeWork

---
+++ src/main/java/spb/alex/lesson1/HomeWork1.java
@@ -0,0 +1,12 @@
+package spb.alex.lesson1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Good Morning!");
+        task();
+    }
+
+    private static void task() {
+        System.out.println("Good Afternoon!");
+    }
+}
