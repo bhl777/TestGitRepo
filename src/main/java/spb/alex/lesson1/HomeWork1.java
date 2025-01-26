4ad3fdabc7c9679a9e1379b9b9598c21ef5c174e
alex_a <admin@modnica.com>
2025-01-26 20:10:26 +0300

    some changes

--- src/main/java/spb/alex/lesson1/HomeWork1.java
+++ src/main/java/spb/alex/lesson1/HomeWork1.java
@@ -4,6 +4,7 @@ public class HomeWork1 {
     public static void main(String[] args) {
         System.out.println("Good Morning!");
         task();
+        //some changes
     }
 
     private static void task() {
